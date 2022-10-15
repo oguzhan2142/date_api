a6f67ce799492f2ef9c08a1d309974ff5dd83d04
Oğuzhan Topal <oguzhan2142@hotmail.com>
2022-10-15 11:24:55 +0300

    get next user & result match

--- routes/match_route.js
+++ routes/match_route.js
@@ -4,18 +4,70 @@ const router = express.Router();
 
 const User = require("../model/user");
 
-router.get("/", async (req, res) => {
+router.get("/next", async (req, res) => {
   const userId = req.query.userId;
   console.log(userId);
   const user = await User.findById(userId);
 
-  console.log(user.username);
+  let excludeIdList = [userId];
 
-  const candidates = await User.find({ _id: { $ne: user.id } });
+  if (user.acceptedUsers != null) {
+    excludeIdList = [...excludeIdList, ...user.acceptedUsers];
+  }
 
-  const candidate = candidates[0];
+  console.log(`rejectedUser : ${user.rejectUsers}`);
 
+  if (user.rejectUsers != null) {
+    excludeIdList = [...excludeIdList, ...user.rejectUsers];
+  }
+  console.log(`excludeIdList : ${excludeIdList}`);
+  const candidate = await User.findById({ $nin: excludeIdList });
+
+  if (candidate == null) {
+    return res.status(404).json({ message: "You consumed all people nearby" });
+  }
+  console.log(`user: ${user.username} : ${user.id}`);
+  console.log(`candidate: ${candidate.username} : ${candidate.id}`);
   res.json(candidate);
+});
+
+router.post("/answer", async (req, res) => {
+  const userId = req.body.userId;
+  const targetUserId = req.body.targetUserId;
+  const isAccepted = req.body.isAccepted;
+
+  const user = await User.findById({ _id: userId });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  if (isAccepted == undefined) {
+    res.status(400).json({ message: "isAccepted is required" });
+  }
+
+  let modifiedUser;
+  if (isAccepted) {
+    const list = user.acceptedUsers;
+    if (list.includes(targetUserId)) {
+      return res.status(400).json({ message: "Already accepted" });
+    }
+    list.push(targetUserId);
+    modifiedUser = await User.updateOne(
+      { _id: userId },
+      { acceptedUsers: list }
+    );
+  } else {
+    const list = user.rejectUsers;
+    if (list.includes(targetUserId)) {
+      return res.status(400).json({ message: "Already rejected" });
+    }
+
+    list.push(targetUserId);
+    modifiedUser = await User.updateOne({ _id: userId }, { rejectUsers: list });
+  }
+
+  res.json(modifiedUser);
 });
 
 module.exports = router;
